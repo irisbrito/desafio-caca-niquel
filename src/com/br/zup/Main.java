0e509d4d6ab24189c78fd6b476f40511415d1fd0
Iris <iris.souza@zup.com.br>
2021-02-12 13:19:07 -0300

    Criando métodos IO

---
+++ src/com/br/zup/Main.java
@@ -0,0 +1,14 @@
+package com.br.zup;
+/*
+ Desafio 1 - Criar um sistema que simula um caça níquel.
+ O Sistema deve gerar 3 valores aleatórios entre as opções
+ Banana - 10 pontos, Morango - 20 pontos e Estrela - 40 pontos.
+ Sempre que é executado os valores devem ser somados e apresentado ao usuário o valor total da pontuação
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
